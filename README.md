3efc038a006ba17eb9c3600f4ef9aa6cebd8d1c8
AbdulazizKhader <akhader@ucsd.edu>
2024-09-13 15:23:11 -0700

    separated the functionality to download the video from the main logic and added sample videos

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 YouTubeOffline is a Python program that will allow you to download YouTube videos and watch them on the go without having to connect to the internet. 
 
-From the command line, you will be able to download YouTube videos by the link to the video, and it will print out details about the given video as well as .
+From the command line, you will be able to download YouTube videos by the link to the video, and it will print out details about the given video as well as download the video. You can input a list of YouTube links into the terminal directly or put them into a text file and, when prompted, input the path to the text file.
 
-This program was originally designed to use the PyTube library, but recently there was a HTTP error that was raised by the internal servers of PyTube. To fix that PyTube recommended us to use PyTubeFix instead.
+This program was originally designed to use the PyTube library, but recently there was a HTTP error that was raised by the internal servers of PyTube. To fix that PyTube recommended us to use PyTubeFix instead. As of right now, the program only downloads the video file without the audio.
--- youTubeDownloader.py
+++ youTubeDownloader.py
@@ -1,17 +1,39 @@
 from pytubefix import YouTube
 from sys import argv
+import os.path
+import sys
 
-for i in range(1, len(argv)):
-    link = argv[i]
+def download_video(link, pathToSave, number):
     yt = YouTube(link)
-
     print("Title: ", yt.title)
     print("Views: ", yt.views)
-
     ytd = yt.streams.get_highest_resolution()
-    message = "Downloading video number " + str(i) + "..."
+    
+    message = "Downloading video number " + str(number) + "..."
     print(message)
-    ytd.download("./") #Change to desired path
-    print("Done!")
+    ytd.download(pathToSave) 
+    print("Done! \n")
 
+#Checking what type of input we are getting
+vid_location = input("Are the video(s) in a file or will you insert them here, into the terminal? Answer file or terminal. \n")
+path_to_save = input("Leave blank if you want to download the file in this directory or enter the desired path: \n") or "./"
 
+if vid_location == "terminal":
+    for i in range(1, len(argv)):
+        link = argv[i]
+        download_video(link, path_to_save, i)
+        
+elif vid_location == "file":
+    file_path = input("Please input the path to the text file that contains the YT links: \n")
+    if not os.path.isfile(file_path): 
+        sys.exit("The given path is not a file. Please enter a valid path.")
+    
+    file = open(file_path)
+    video_number = 1
+    
+    for line in file:
+        download_video(line, path_to_save, video_number)
+        video_number += 1
+        
+else:
+    print("\nNot a valid input. Please put the YT links in a list or place them in a text file.")
