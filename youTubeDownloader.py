6d0616834b111f65ab357fa8192f42365b94e77f
AbdulazizKhader <akhader@ucsd.edu>
2024-09-13 05:04:08 -0700

    Added functionality to download multiple videos at the same time

--- youTubeDownloader.py
+++ youTubeDownloader.py
@@ -1,13 +1,17 @@
 from pytubefix import YouTube
 from sys import argv
 
-link = argv[1]
-yt = YouTube(link)
+for i in range(1, len(argv)):
+    link = argv[i]
+    yt = YouTube(link)
 
-print("Title: ", yt.title)
-print("Views: ", yt.views)
+    print("Title: ", yt.title)
+    print("Views: ", yt.views)
 
-ytd = yt.streams.get_highest_resolution()
-print("Downloading video...")
-ytd.download("./")
-print("Done!")
+    ytd = yt.streams.get_highest_resolution()
+    message = "Downloading video number " + str(i) + "..."
+    print(message)
+    ytd.download("./") #Change to desired path
+    print("Done!")
+
+
